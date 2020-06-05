054a3d6070c93482d5008b28a45356b6b10b3dcb
Maria Luiza <malualper@hotmail.com>
2020-06-05 18:42:30 -0300

    calculadoraidade

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace calculadoraidade
+
+//Calcule a sua idade em dias.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            
+            
+
+            Console.Write("Digite sua data de nascimento:");
+            DateTime nascimento=Convert.ToDateTime(Console.ReadLine());
+            DateTime agora = DateTime.Now;
+            TimeSpan DiasDeVida= agora - nascimento;
+            int dias= DiasDeVida.Days;
+            Console.WriteLine($"Você tem: " + dias+ " dias de vida");
+
+    }
+}
+}
